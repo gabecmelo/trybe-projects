fbdd1320682134944dfd595113cfb8c15594bd0d
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-06-20 17:07:15 -0300

    apply clipboard-copy

--- Gerador de Senhas Seguras/src/main.js
+++ Gerador de Senhas Seguras/src/main.js
@@ -1,10 +1,16 @@
 import { nanoid } from "nanoid";
 import "./style.css";
+import copy from "clipboard-copy";
 
 const button = document.querySelector("button");
 const displayPass = document.querySelector("h2");
 
-button.addEventListener("click", () => {  
+button.addEventListener("click", () => {
     const randomSafePass = nanoid();
     displayPass.innerHTML = randomSafePass;
+});
+
+displayPass.addEventListener("click", (event) => {
+    copy(event.target.innerHTML);
+    alert("Sua senha foi copiada com sucesso: " + displayPass.innerText);
 });
