7e6be4c5e23bf86e3e77f6a021129fcccc2ecca5
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-06-20 16:49:38 -0300

    apply styles

--- Gerador de Senhas Seguras/src/main.js
+++ Gerador de Senhas Seguras/src/main.js
@@ -1,1 +1,10 @@
-console.log('Olá, Mundo!');
+import { nanoid } from "nanoid";
+import "./style.css";
+
+const button = document.querySelector("button");
+const displayPass = document.querySelector("h2");
+
+button.addEventListener("click", () => {  
+    const randomSafePass = nanoid();
+    displayPass.innerHTML = randomSafePass;
+});
